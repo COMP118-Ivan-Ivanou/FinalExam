c15abdad64d95521905902aeb1cccdda6312f04a
janisivanoov <janisivanov@icloud.com>
2022-06-01 12:37:34 +0300

    unit test changes

--- UnitTest/UnitTest.cpp
+++ UnitTest/UnitTest.cpp
@@ -12,7 +12,20 @@ namespace UnitTest
 		
 		TEST_METHOD(TestMethod1)
 		{
+			srand(time(NULL));
 
+			double matrix[4][3];
+
+			for (int i = 0; i < 4; i++) {
+				for (int y = 0; y < 3; y++) {
+					int num = rand();
+					matrix[i][y] = num;
+
+					Assert:isTrue(i == 1.0, matrix[i][y]);
+					Assert:isFalse(i == 3.0, matrix[i][y]);
+					Assert:notEqual(i == 2.0, matrix[i][y]);
+				}
+			}
 		}
 	};
 }
